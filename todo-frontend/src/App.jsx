4bf49f4b8af26248307058785c58f79c1092b8e5
AndyWeasley2004 <m5yao@ucsd.edu>
2023-07-29 21:26:23 -0700

    adding todo list display page

--- todo-frontend/src/App.jsx
+++ todo-frontend/src/App.jsx
@@ -1,11 +1,18 @@
 
 import './App.css'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import ListTodoComponent from "./components/ListTodoComponent.jsx";
 
 function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element = { <ListTodoComponent /> }></Route>
+          <Route path='/todos' element = { <ListTodoComponent /> }></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- todo-frontend/src/components/ListTodoComponent.jsx
+++ todo-frontend/src/components/ListTodoComponent.jsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {getAllTodos} from "../services/TodoService.js";
 
 const ListTodoComponent = () => {
 
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        listTodos();
+    }, [])
+
+    function listTodos(){
+        getAllTodos().then((response) => {
+            setTodos(response.data);
+        }).catch(error => {
+            console.error(error);
+        });
+    }
 
     return (
-        <h1>Hello World</h1>
+        <div className='container'>
+            <h2 className='text-center'>List of Todos</h2>
+            <div>
+                <table className='table table-striped table-bordered' >
+                    <thead>
+                        <tr>
+                            <th>Todo Title</th>
+                            <th>Todo Description</th>
+                            <th>Todo Completed</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        todos.map(todo =>
+                            <tr key={todo.id}>
+                                <td>{todo.title}</td>
+                                <td>{todo.description}</td>
+                                <td>{todo.completed ? 'YES' : 'NO'}</td>
+                            </tr>
+                        )
+                    }
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
     )
 }
 
