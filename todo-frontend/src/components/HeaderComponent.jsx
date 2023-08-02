6f844c48cd6bfd51c9caa964c1b4c45a11aed7a7
AndyWeasley2004 <m5yao@ucsd.edu>
2023-08-01 20:10:07 -0700

    Adding security features: cannot directly access add/update/list todos if not logged in

--- todo-frontend/src/components/HeaderComponent.jsx
+++ todo-frontend/src/components/HeaderComponent.jsx
@@ -1,31 +1,62 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
+import {isUserLoggedIn, logout} from "../services/AuthService.js";
 
 
 const HeaderComponent = () => {
+
+    const isAuth = isUserLoggedIn();
+
+    const navigator = useNavigate();
+
+    function handleLogout(){
+        logout();
+
+        navigator('/login');
+    }
+
+
     return (
         <div>
             <header>
                 <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
                     <div>
-                        <a href='http://localhost:3000' className='navbar-brand' style={{marginLeft: "10px"}}>Todo Management System</a>
+                        <a href='http://localhost:3000' className='navbar-brand' style={{marginLeft: "10px"}}>Todo
+                            Management System</a>
                     </div>
                     <div className="collapse navbar-collapse">
                         <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <NavLink to='/todos' className='nav-link'>Todos</NavLink>
-                            </li>
+
+                            {
+                                isAuth &&
+                                <li className="nav-item">
+                                    <NavLink to='/todos' className='nav-link'>Todos</NavLink>
+                                </li>
+                            }
+
                         </ul>
+
                     </div>
-                        <ul className="navbar-nav">
+                    <ul className="navbar-nav">
+                        {
+                            !isAuth &&
                             <li className="nav-item">
                                 <NavLink to='/register' className='nav-link'>Register</NavLink>
                             </li>
-
+                        }
+                        {
+                            !isAuth &&
                             <li className="nav-item">
                                 <NavLink to='/login' className='nav-link'>Log In</NavLink>
                             </li>
-                        </ul>
+                        }
+                        {
+                            isAuth &&
+                            <li className="nav-item">
+                                <NavLink to='/login' className='nav-link' onClick={handleLogout}>Logout</NavLink>
+                            </li>
+                        }
+                    </ul>
                 </nav>
 
             </header>
--- todo-frontend/src/components/LoginComponent.jsx
+++ todo-frontend/src/components/LoginComponent.jsx
@@ -1,24 +1,27 @@
 import React, {useState} from "react";
-import {loginAPICall, storeToken} from "../services/AuthService.js";
+import {loginAPICall, saveLoggedInUser, storeToken} from "../services/AuthService.js";
 import {useNavigate} from "react-router-dom";
 
 function LoginComponent() {
 
-    const [username , setUsername] = useState('');
-    const [password , setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
     const navigate = useNavigate();
 
-    function handleLoginForm(e){
+    async function handleLoginForm(e) {
         e.preventDefault();
 
-        loginAPICall(username, password).then((response) => {
+        await loginAPICall(username, password).then((response) => {
             console.log(response.data);
 
-            const token = 'Basic ' + window.btoa(username + ':' + password);
+            const token = 'Basic ' + window.btoa(username + ":" + password);
             storeToken(token);
 
-            navigate('/todos');
+            saveLoggedInUser(username);
+            navigate("/todos")
+
+            window.location.reload(false);
         }).catch(error => {
             console.error(error);
         })
@@ -29,42 +32,52 @@ function LoginComponent() {
         <div className='container'>
             <br/><br/>
             <div className="row">
-                <div className="card col-md-6 offset-md-3 offset-md-3">
-                    <div className='card-header'>
-                        <h2 className='text-center'>Log In</h2>
+                <div className="col-md-6 offset-md-3 offset-md-3">
+                    <div className='card'>
+                        <div className='card-header'>
+                            <h2 className='text-center'>Login Form</h2>
+                        </div>
+                        <div className="card-body">
+                            <form>
+                                <div className='row mb-3'>
+                                    <label className='form-label col-md-3 '>Username Or Email:</label>
+                                    <div className='col-md-9'>
+                                        <input
+                                            type='text'
+                                            placeholder="Enter Username"
+                                            className='form-control'
+                                            name='username'
+                                            value={username}
+                                            onChange={(e) => setUsername(e.target.value)}
+                                        />
+                                    </div>
+
+                                </div>
+
+                                <div className='row mb-3'>
+                                    <label className='form-label col-md-3'>Password:</label>
+                                    <div className='col-md-9'>
+                                        <input
+                                            type='text'
+                                            placeholder="Enter Password"
+                                            className='form-control'
+                                            name='password'
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                        />
+                                    </div>
+
+                                </div>
+
+                                <div className='form-group mb-3'>
+                                    <button className='btn btn-primary' onClick={(e) => handleLoginForm(e)}>Submit
+                                    </button>
+
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                    <div className="card-body">
-                        <form>
-                            <div className='row mb-3'>
-                                <label className='control-lavel col-md-3'>Username Or Email:</label>
-                                <input
-                                    type='text'
-                                    placeholder="Enter Username"
-                                    className='form-control'
-                                    name='username'
-                                    value={username}
-                                    onChange={(e) => setUsername(e.target.value)}
-                                />
-                            </div>
 
-                            <div className='row mb-3'>
-                                <label className='control-lavel col-md-3'>Password:</label>
-                                <input
-                                    type='text'
-                                    placeholder="Enter Password"
-                                    className='form-control'
-                                    name='password'
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                />
-                            </div>
-
-                            <div className='form-group mb-3'>
-                                <button className='btn btn-primary' onClick={(e) => handleLoginForm(e)}>Submit</button>
-
-                            </div>
-                        </form>
-                    </div>
                 </div>
             </div>
         </div>
